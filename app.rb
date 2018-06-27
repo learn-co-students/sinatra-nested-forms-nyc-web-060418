10f15c93addf82770213dce24afe06689c36f221
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-06-27 16:37:43 -0400

    add forms and routes

--- app.rb
+++ app.rb
@@ -1,9 +1,31 @@
 require './environment'
+require 'pry'
 
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/new' do
+      erb :'/pirates/new'
+    end
+
+    post '/pirates' do
+
+      pirate_args = {name: params["pirate"]["name"], weight: params["pirate"]["weight"], height: params["pirate"]["height"]}
+      boat1_args = {name: params["boat1"]["name"], type: params["boat1"]["type"], booty: params["boat1"]["booty"]}
+      boat2_args = {name: params["boat2"]["name"], type: params["boat2"]["type"], booty: params["boat2"]["booty"]}
+
+
+      @pirate = Pirate.new(pirate_args)
+      @boat1 = Ship.new(boat1_args)
+      @boat2 = Ship.new(boat2_args)
+
+      erb :'/pirates/show'
+    end
+
+    get '/pirates' do
+
+      erb :'/pirates/show'
+    end
 
   end
 end
